0da8864482c41149af5598c3221ba1a916885b25
Gunnar <gunnartl@uio.no>
2019-02-17 00:56:57 +0100

    Commit before test on abel

--- jobscript.sh
+++ jobscript.sh
@@ -7,7 +7,7 @@
 #SBATCH --account=nn9565k
 #
 # Wall clock limit:
-#SBATCH --time=04:30:01     
+#SBATCH --time=02:00:01     
 #
 # Max memory usage:
 #SBATCH --mem-per-cpu=2G
--- run_jobs.sh
+++ run_jobs.sh
@@ -5,6 +5,6 @@ params_path=$(python set_parameters.py abel)
 export params_path
 cd ..
 #sbatch --array=0-7 n_jobs jobscript.sh
-sbatch --array=0-31 jobscript.sh 
+#sbatch --array=0-31 jobscript.sh 
 
-
+sbatch --array=1 jobscript.sh
